65fea36c0ad4f0f57990fbc4bc6191af572df988
Marcus Perdue <marcusperduee@gmail.com>
2023-08-31 18:01:57 -0700

    added common js that info.html can use to reload

---
+++ Assets/js/common.js
@@ -0,0 +1,20 @@
+
+//helps reload on info page
+//index.html and info.html need  this <script src="/Assets/js/common.js"></script>
+
+$(document).ready(function() {
+    $('#searchButton').click(function() {
+        var searchTerm = $('#search').val();
+        if (searchTerm) {
+            var omdbApiKey = 'ca388ffd';
+            var omdbApiUrl = 'https://www.omdbapi.com/?apikey=' + omdbApiKey + '&t=' + encodeURIComponent(searchTerm);
+
+            $.getJSON(omdbApiUrl, function(data) {
+                if (data.Response === 'True') {
+                    localStorage.setItem('movieData', JSON.stringify(data));
+                    window.location.href = 'info.html';
+                }
+            });
+        }
+    });
+});
